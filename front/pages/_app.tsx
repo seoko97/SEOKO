b10a7944f18bae4dc0f7b54b7c87553bcb346ac0
seokho10007 <sukho1007@naver.com>
2021-10-23 23:15:02 +0900

    Create Next Typescript App

---
+++ front/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+	return (
+		<>
+			<Component {...pageProps} />
+		</>
+	);
+};
+export default MyApp;
