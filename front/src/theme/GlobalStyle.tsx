a140240d9ff8649feb118732bc53af8243a8c176
seokho10007 <sukho1007@naver.com>
2021-10-26 22:18:25 +0900

    Style. Emotion Theme 및 GlobalStyle 추가

---
+++ front/src/theme/GlobalStyle.tsx
@@ -0,0 +1,50 @@
+import { Global, css } from "@emotion/react";
+import reset from "emotion-reset";
+import { ThemeType } from ".";
+
+interface GlobalProps {
+  theme: ThemeType;
+}
+
+const GlobalStyle = ({ theme }: GlobalProps) => (
+  <Global
+    styles={css`
+      ${reset}
+
+      html, body, #__next {
+        height: 100%;
+      }
+      html,
+      body {
+        overflow-x: hidden;
+        width: 100%;
+        height: 100%;
+      }
+      body {
+        overflow-y: overlay;
+        margin: 0;
+        font-size: 14px;
+        line-height: 1.5715;
+        user-select: none;
+      }
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        margin-top: 0;
+        margin-bottom: 0.5em;
+      }
+      a {
+        text-decoration: none;
+        outline: none;
+      }
+      & * {
+        font-family: "Noto Sans KR", sans-serif !important;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
