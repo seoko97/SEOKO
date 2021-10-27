e6c62892ac0b4927699f5afb35cd72e707b4f2c0
seokho10007 <sukho1007@naver.com>
2021-10-27 23:04:27 +0900

    Feat. Storybbok 절대 경로 추가 및 GlobalStyle 적용

---
+++ front/.storybook/preview.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { css, Global, ThemeProvider }  from '@emotion/react'
+import reset from 'emotion-reset'
+import { lightTheme } from "@theme/.";
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${reset}
+
+      html, body, #__next {
+        height: 100%;
+      }
+      html,
+      body {
+        overflow-x: hidden;
+        width: 100%;
+        height: 100%;
+      }
+      body {
+        overflow-y: overlay;
+        margin: 0;
+        font-size: 14px;
+        line-height: 1.5715;
+        user-select: none;
+      }
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        margin-top: 0;
+        margin-bottom: 0.5em;
+      }
+      a {
+        text-decoration: none;
+        outline: none;
+      }
+      & * {
+        font-family: "Noto Sans KR", sans-serif !important;
+      }
+    `}
+  />
+);
+
+// storybook 실행시 적용되는 스타일
+export const decorater = [
+  (Story) => (
+    <ThemeProvider theme={lightTheme}>
+      <Story />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+]
+
+export const parameters = {
+  backgrounds: {
+    default: "white",
+    values: [
+      {
+        name: "white",
+        value: "#ffffff",
+      },
+      {
+        name: "dark",
+        value: "#333333",
+      },
+    ],
+  },
+};
