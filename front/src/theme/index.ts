3f2877bd426bfeed22c1f8e7e9c2be89b991e599
seokho10007 <sukho1007@naver.com>
2021-10-28 23:25:28 +0900

    Style. DarkMode Theme Default Style

--- front/src/theme/index.ts
+++ front/src/theme/index.ts
@@ -6,10 +6,28 @@ const bp = {
 };
 
 export const lightTheme = {
+  BAKCGROUND_COLOR: {
+    PRIMARY_COLOR: "#F2F5F7",
+    SECONDARY_COLOR: "hsl(0, 0%, 97%)",
+  },
+  FONT_COLOR: {
+    PRIMARY_COLOR: "#212529",
+    SECOND_COLOR: "#1b2e46",
+    LOGO_COLOR: "#bda6d2",
+  },
   BP: bp,
 };
 
 export const darkTheme = {
+  BAKCGROUND_COLOR: {
+    PRIMARY_COLOR: "#212529",
+    SECONDARY_COLOR: "hsl(0, 0%, 41%)",
+  },
+  FONT_COLOR: {
+    PRIMARY_COLOR: "#f8f9fa",
+    SECONDARY_COLOR: "#95a1b2",
+    LOGO_COLOR: "#c58ff7",
+  },
   BP: bp,
 };
 
