61dd92c10079ba510a6e485b36ee51cb2f8194f1
seokho10007 <sukho1007@naver.com>
2021-10-28 23:22:25 +0900

    Style. 공통 레이아웃 작성

---
+++ front/src/components/UI/frames/AppLayout/AppLayout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import Footer from "@organisms/Footer";
+import Header from "@organisms/Header";
+
+const StyledAppLayout = styled.div`
+  min-height: calc(100% - 140px);
+  position: relative;
+  padding-bottom: 140px;
+  background-color: ${({ theme }) => theme.BAKCGROUND_COLOR.PRIMARY_COLOR};
+  transition: background 0.3s;
+`;
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AppLayout = ({ children }: Props) => {
+  return (
+    <>
+      <StyledAppLayout>
+        <Header />
+        {children}
+        <Footer />
+      </StyledAppLayout>
+    </>
+  );
+};
+
+export default AppLayout;
