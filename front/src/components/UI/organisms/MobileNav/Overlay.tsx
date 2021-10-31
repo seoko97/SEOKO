60d183885c965870e7fe8ed378aaa3261d5f1b0e
seokho10007 <sukho1007@naver.com>
2021-10-31 22:55:45 +0900

    Feat. Menu 클릭시 스크롤 이동을 막아주는 Overlay 생성

---
+++ front/src/components/UI/organisms/MobileNav/Overlay.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { MenuProps } from "./MobileNav";
+
+const StyledOverlay = styled.div<{ navState: boolean }>`
+  display: ${({ navState }) => (navState ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.BAKCGROUND_COLOR.PRIMARY_COLOR_RGBA};
+  z-index: 2;
+`;
+
+const Overlay = ({ navState, openMenu }: MenuProps) => (
+  <StyledOverlay onClick={openMenu} navState={navState} />
+);
+
+export default Overlay;
