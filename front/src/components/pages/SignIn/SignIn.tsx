b3f6b6d91c57f638e3a4226a6089528c8e4b19cb
seokho10007 <sukho1007@naver.com>
2021-11-03 21:10:00 +0900

    SignIn Component 추가

---
+++ front/src/components/pages/SignIn/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInForm from "@organisms/SignInForm";
+
+const SignIn = () => {
+  return (
+    <>
+      <SignInForm />
+    </>
+  );
+};
+
+export default SignIn;
