bb7ebb00da2d4d3625e9e4ab83b4f6fb1900956c
seokho10007 <sukho1007@naver.com>
2021-11-04 23:45:39 +0900

    Feat. User Params Custom Decorator 생성

---
+++ back/src/decorators/user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export interface TokenUser {
+  id: number;
+}
+
+export const User = createParamDecorator((_, context: ExecutionContext) => {
+  const req = context.switchToHttp().getRequest();
+
+  return req.user;
+});
