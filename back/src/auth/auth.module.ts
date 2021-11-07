3b291cc40639d837c085c769e5afb705ec15e52e
seokho10007 <sukho1007@naver.com>
2021-11-07 23:21:57 +0900

    Feat. JWT 인증 설정

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+
+import { UsersModule } from "@users/users.module";
+import { AuthService } from "./auth.service";
+import { jwtContents } from "./contents";
+import { LocalStrategy } from "./strategies/local.strategy";
+import { ExpriedJwtStrategy, JwtStrategy } from "./strategies/jwt.strategy";
+import { AuthController } from "./auth.controller";
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtContents.secret,
+      signOptions: { expiresIn: "10m" },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy, ExpriedJwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { TokenUser } from "@decorators/user.decorator";
 import { UsersService } from "@users/users.service";
+import { encryptValue } from "@utils/crypto";
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService, private jwtService: JwtService) {}
 
   async validateuser(userId: string, password: string): Promise<any> {
-    //   const user = await this.userService.get
+    const user = await this.userService.getByUserId(userId);
+
+    const isCompare = await user?.comparePassword(password);
+
+    if (!user || (user && !isCompare)) return null;
+
+    const result = await this.userService.getById(user.id);
+
+    return result;
+  }
+
+  async signIn(payload: TokenUser): Promise<string> {
+    const accessToken = this.jwtService.sign(payload, { expiresIn: "10m" });
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: "14d" });
+
+    await this.userService.updateRefreshToken(payload.id, refreshToken);
+
+    return encryptValue(accessToken);
+  }
+
+  async verifyRefresh(payload: TokenUser): Promise<boolean> {
+    const user = await this.userService.getById(payload.id);
+    if (!user) return false;
+    return user.verifyRefresh();
   }
 }
