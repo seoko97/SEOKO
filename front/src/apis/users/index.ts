390f893ed9afd5fdac53739262357980a0a23073
seokho10007 <sukho1007@naver.com>
2021-11-10 23:45:14 +0900

    Feat. 로그인 여부 확인을 위한 initatialSigninCheck 함수 생성

--- front/src/apis/users/index.ts
+++ front/src/apis/users/index.ts
@@ -1,8 +1,26 @@
-import { SignUpUser } from "@src/types/users";
+import { SignInUser, SignUpUser } from "@src/types/users";
 import axios from "../.";
 
 export const onSignup = async (data: SignUpUser) => {
   const result = await axios.post("users/signup", data).then((res) => res.data);
 
-  if (result.pass) return result.username;
+  return result;
+};
+
+export const onSignout = async () => {
+  const result = await axios.post("auth/signout").then((res) => res.data);
+
+  return result;
+};
+
+export const onSignin = async (data: SignInUser) => {
+  const result = await axios.post("auth/signin", data).then((res) => res.data);
+
+  return result;
+};
+
+export const initatialSigninCheck = async () => {
+  const result = await axios.get("users/info").then((res) => res.data);
+
+  return result;
 };
