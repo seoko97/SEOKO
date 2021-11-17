6e592ad62d9f0ec31eb78aa04d9fc091cde531ac
seokho10007 <sukho1007@naver.com>
2021-11-17 22:56:43 +0900

    Chore. Post Frame Style 추가

--- front/src/components/UI/molecules/PostItem/PostItem.tsx
+++ front/src/components/UI/molecules/PostItem/PostItem.tsx
@@ -1,14 +1,53 @@
 import React from "react";
+import Image from "next/image";
+import styled from "@emotion/styled";
 import { Props as PostProps } from "@states/posts/atoms";
 
 interface Props {
   post: PostProps;
 }
 
+const StyedPostItem = styled.div`
+  width: 100%;
+  height: 220px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30px;
+  background-color: ${({ theme }) => theme.BAKCGROUND_COLOR.SECONDARY_COLOR};
+  border-radius: 5px;
+  box-shadow: 0 1px 6px 0 hsla(0, 0%, 0%, 0.1);
+
+  & .img_box {
+    width: 30%;
+    height: 100%;
+
+    & img {
+      width: 100%;
+      height: 100%;
+      border-radius: 5px 0 0 5px;
+    }
+  }
+  & .content_box {
+    width: 70%;
+    padding: 10px;
+  }
+`;
+
 const PostItem = ({ post }: Props) => {
+  console.log(post);
   return (
     <>
-      <div>asd</div>
+      <StyedPostItem>
+        <div className="img_box">
+          <img src={post.titleImage} />
+        </div>
+        <div className="content_box">
+          <h1>{post.title}</h1>
+          <h1>{post.title}</h1>
+          <h1>{post.title}</h1>
+        </div>
+      </StyedPostItem>
     </>
   );
 };
--- front/src/components/UI/organisms/PostList/PostList.tsx
+++ front/src/components/UI/organisms/PostList/PostList.tsx
@@ -1,8 +1,19 @@
 import React, { useEffect } from "react";
 import { useRecoilState } from "recoil";
+import styled from "@emotion/styled";
 
 import { postState } from "@states/posts/atoms";
 import { post as postList } from "@src/dummy/posts";
+import RowFrame from "@frames/RowFrame";
+import PostItem from "@molecules/PostItem";
+
+const StyledPostList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const PostList = () => {
   const [posts, setPosts] = useRecoilState(postState);
@@ -13,7 +24,11 @@ const PostList = () => {
 
   return (
     <>
-      <div>{posts && posts.map((el) => <div key={el.id + el.title}>asd</div>)}</div>
+      <StyledPostList>
+        <RowFrame>
+          {posts && posts.map((el) => <PostItem key={el.id + el.title} post={el} />)}
+        </RowFrame>
+      </StyledPostList>
     </>
   );
 };
