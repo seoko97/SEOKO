abb54446b93e44856d863bb6abee313032fa5ee9
seokho10007 <sukho1007@naver.com>
2021-12-29 23:39:28 +0900

    Feat. 사용하지 않는 모듈 제거

--- front/src/components/pages/About/About.tsx
+++ front/src/components/pages/About/About.tsx
@@ -4,10 +4,6 @@ import ParallaxForm from "@molecules/ParallaxForm";
 const About = () => {
   return (
     <>
-      <ParallaxForm imgSrc="/about.jpg">
-        <h2>About Me</h2>
-        <p>성장하는 개발자가 되고싶은 지석호입니다.</p>
-      </ParallaxForm>
       <div>asdasd</div>
     </>
   );
