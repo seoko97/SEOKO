901daad91c21c81e465e6e7a2b62ddb9cdd82520
seokho10007 <sukho1007@naver.com>
2022-01-08 23:35:53 +0900

    Chore. Card Style 모듈화

--- front/src/components/UI/atoms/Card/Card.tsx
+++ front/src/components/UI/atoms/Card/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const StyledCard = styled.div`
+export const StyledCard = styled.div`
   background-color: ${({ theme }) => theme.BAKCGROUND_COLOR.SECONDARY_COLOR};
   box-shadow: 0 1px 6px 0 hsla(0, 0%, 0%, 0.1);
 `;
