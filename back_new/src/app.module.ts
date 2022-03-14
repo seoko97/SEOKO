98d2c80f15572797aea5143301951c4ae1abfc9a
seokho10007 <sukho1007@naver.com>
2022-03-14 23:55:29 +0900

    Feat. Module 추가

--- back_new/src/app.module.ts
+++ back_new/src/app.module.ts
@@ -1,41 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import { UserModule } from '@src/users/user.module';
-import { databaseProviders } from './db/db.providers';
-import { userProviders } from './users/user.providers';
-import { UserService } from './users/user.service';
-import { UserResolver } from './users/user.resolver';
-
-const prod = process.env.NODE_ENV === 'production';
+import { UserModule } from '@users/user.module';
+import { UserService } from '@users/user.service';
+import { UserResolver } from '@users/user.resolver';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      playground: !prod,
-      debug: !prod,
-      autoSchemaFile: join(process.cwd(), 'schema.graphql'),
-      sortSchema: true,
-      cors: {
-        credentials: true,
-        origin: true,
-      },
-      context: (ctx) => ({ ...ctx }),
-      typePaths: ['./**/*.graphql'],
-    }),
-    UserModule,
-  ],
-  providers: [
-    UserService,
-    UserResolver,
-    ...databaseProviders,
-    ...userProviders,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule, UserModule],
 })
 export class AppModule {}
