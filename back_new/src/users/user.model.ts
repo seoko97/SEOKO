483bc7697edc70bcfe1d91b5d362aebdca553537
seokho10007 <sukho1007@naver.com>
2022-03-22 23:34:24 +0900

    Feat. User Schema Method 생성 및 Field 추가

--- back_new/src/users/user.model.ts
+++ back_new/src/users/user.model.ts
@@ -1,17 +1,67 @@
 import { ObjectType, Field, InputType } from '@nestjs/graphql';
-import { Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Schema as MongooseSchema, Document } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { verify } from 'jsonwebtoken';
+import { jwtContents } from '@auth/contents';
 
+const BCRYPT_SALT = 10 as const;
+export type UserDocument = User & Document;
 @Schema()
 @InputType('UserModel', { isAbstract: true })
 @ObjectType()
-export class User {
+export class User extends Document {
   @Field(() => String)
   _id: MongooseSchema.Types.ObjectId;
 
-  @Prop()
+  @Prop({ required: true, unique: true })
   @Field(() => String, { description: 'User Name' })
   name: string;
+
+  @Prop({ required: true, unique: true })
+  @Field(() => String, { description: 'User ID' })
+  userId: string;
+
+  @Prop({ required: true })
+  @Field(() => String, { description: 'User Password' })
+  password: string;
+
+  @Prop({ default: null })
+  @Field(() => String, { description: 'User Refresh Roken' })
+  refreshToken?: string;
+
+  @Prop({
+    methods: Function,
+  })
+  verifyRefresh: () => boolean;
+
+  @Prop({
+    methods: Function,
+  })
+  comparePassword: (aPassword: string) => boolean;
+
+  @Prop({
+    statics: Function,
+  })
+  hashPassword: (userData: User) => void;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.verifyRefresh = function () {
+  if (!this.refreshToken) return false;
+  const result = verify(this.refreshToken, jwtContents.secret);
+
+  return Boolean(result);
+};
+
+UserSchema.methods.comparePassword = async function (aPassword) {
+  const isCompare = await bcrypt.compare(aPassword, this.password);
+
+  return isCompare;
+};
+
+UserSchema.statics.hashPassword = async function (userData: User) {
+  if (userData.password)
+    userData.password = await bcrypt.hash(userData.password, BCRYPT_SALT);
+};
