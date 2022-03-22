714afea813e2cd2e6b77ddb5d96cdb53e3a545b9
seokho10007 <sukho1007@naver.com>
2022-03-22 23:35:48 +0900

    Feat. User Create를 위한 DTO 생성

--- back_new/src/users/dto/createUser.dto.ts
+++ back_new/src/users/dto/createUser.dto.ts
@@ -2,4 +2,8 @@ import { InputType, PickType } from '@nestjs/graphql';
 import { User } from '../user.model';
 
 @InputType('InputUser')
-export class CreateUserInput extends PickType(User, ['name'] as const) {}
+export class CreateUserInput extends PickType(User, [
+  'name',
+  'password',
+  'userId',
+] as const) {}
