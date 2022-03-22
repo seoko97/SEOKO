bbd04e9e5656288941887f11440017c7a67f1bd8
seokho10007 <sukho1007@naver.com>
2022-03-22 23:35:03 +0900

    Feat. 암호화 모듈 생성

---
+++ back_new/src/utils/crypto.ts
@@ -0,0 +1,8 @@
+import { AES, enc } from 'crypto-js';
+import { secretContents } from '@auth/contents';
+
+export const encryptValue = (value: string) =>
+  AES.encrypt(value, secretContents.signin).toString();
+
+export const decryptValue = (value: string) =>
+  AES.decrypt(value, secretContents.signin).toString(enc.Utf8);
