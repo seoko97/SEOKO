1ad883d75697f7a4484316b4924709646a6e0727
seokho10007 <sukho1007@naver.com>
2022-03-29 00:02:32 +0900

    Feat. create 실행 시 password hash 실행

--- back_new/src/users/user.service.ts
+++ back_new/src/users/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { CoreRes } from '@src/decorators/coreRes.decorator';
 import { CreateUserInput } from './dto/createUser.dto';
-import { User, UserDocument } from './user.model';
+import { User, UserModel } from './user.model';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private userModel: UserModel) {}
 
-  async create(input: CreateUserInput): Promise<User> {
-    return await this.userModel.create(input);
+  async create(input: CreateUserInput): Promise<CoreRes> {
+    await this.userModel.hashPassword(input);
+
+    await this.userModel.create(input);
+    return { ok: true };
   }
 
   async getByUserId(userId: string) {
@@ -22,5 +25,9 @@ export class UserService {
 
   async updateRefreshToken(_id: string, refreshToken: string | null) {
     await this.userModel.updateOne({ _id }, { refreshToken });
+  }
+
+  async test() {
+    return await this.userModel.find();
   }
 }
