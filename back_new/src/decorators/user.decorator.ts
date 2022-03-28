283de250dfc96fed84c1846a4f791d558e28b532
seokho10007 <sukho1007@naver.com>
2022-03-28 23:57:29 +0900

    Feat. Decorator 생성

--- back_new/src/decorators/user.decorator.ts
+++ back_new/src/decorators/user.decorator.ts
@@ -1,3 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
 export interface ITokenUser {
   _id: string;
 }
+
+export const User = createParamDecorator((_, context: ExecutionContext) => {
+  const ctx = GqlExecutionContext.create(context);
+  return ctx.getContext().req.user;
+});
