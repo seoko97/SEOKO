44a68e23dfbd8172da9d5108548abb2a801f8853
seokho10007 <sukho1007@naver.com>
2022-03-29 23:54:00 +0900

    Feat. UseGuards 를 통한 JWT 만료 여부 확인

--- back_new/src/users/user.resolver.ts
+++ back_new/src/users/user.resolver.ts
@@ -1,16 +1,32 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { JwtAuthGuard } from '@src/auth/guards/jwt-auth.guard';
+import { CoreRes } from '@src/decorators/coreRes.decorator';
+import { ITokenUser, User } from '@src/decorators/user.decorator';
+import { ApolloError } from 'apollo-server-express';
 import { CreateUserInput } from './dto/createUser.dto';
-import { User } from './user.model';
+import { User as UserModel } from './user.model';
 import { UserService } from './user.service';
 
 @Resolver('User')
 export class UserResolver {
   constructor(private userService: UserService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => CoreRes)
   async create(@Args('input') input: CreateUserInput) {
-    await this.userService.create(input);
+    const user = await this.userService.getByUserId(input.userId);
 
-    return { ok: true };
+    if (user) throw new Error('이미 존재하는 사용자입니다.');
+    console.log(input);
+
+    return await this.userService.create(input);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [UserModel])
+  async getUserInfo(@User() _user: ITokenUser) {
+    const { username } = await this.userService.getById(_user._id);
+
+    return { ok: true, username };
   }
 }
