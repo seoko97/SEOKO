38e48986b5a538022c865a6862f97126a240aaa9
seokho10007 <sukho1007@naver.com>
2022-03-30 23:55:21 +0900

    Feat. AuthModule 추가

--- back_new/src/app.module.ts
+++ back_new/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '@users/user.module';
+import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
