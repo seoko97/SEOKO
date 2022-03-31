86f50453cdc944f01b24771b347b7500af9ee21a
seokho10007 <sukho1007@naver.com>
2022-03-31 23:50:26 +0900

    Feat. Post Model 생성

---
+++ back_new/src/posts/post.model.ts
@@ -0,0 +1,38 @@
+import { ObjectType, Field, InputType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Tag } from '@src/tags/tag.model';
+import { User } from '@src/users/user.model';
+import { Schema as MongooseSchema, Document, Model } from 'mongoose';
+
+export type PostDocument = Post & Document;
+export type PostModel = Model<PostDocument>;
+
+@Schema({ timestamps: true })
+@InputType('PostModel', { isAbstract: true })
+@ObjectType()
+export class Post {
+  @Field(() => String)
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Prop({ required: true })
+  @Field(() => String)
+  title!: string;
+
+  @Prop({ required: true })
+  @Field(() => String)
+  content!: string;
+
+  @Prop({ required: true })
+  @Field(() => String)
+  category!: string;
+
+  @Prop({ required: true })
+  @Field(() => String)
+  coverImg!: string;
+
+  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId }], ref: 'Tag' })
+  @Field(() => [Tag])
+  tags?: Tag[];
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
