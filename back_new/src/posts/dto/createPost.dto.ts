0ad47498bc30af0f948027fa09bc28179b3bb2c8
seokho10007 <sukho1007@naver.com>
2022-04-01 23:49:23 +0900

    Feat. Create Post DTO 생성

---
+++ back_new/src/posts/dto/createPost.dto.ts
@@ -0,0 +1,11 @@
+import { InputType, PickType } from '@nestjs/graphql';
+import { Post } from '../post.model';
+
+@InputType()
+export class CreatePostInput extends PickType(Post, [
+  'coverImg',
+  'title',
+  'content',
+  'category',
+  'tags',
+]) {}
