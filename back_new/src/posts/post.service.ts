6aaf53dff5068d0549525e653e4665c78004c0a8
seokho10007 <sukho1007@naver.com>
2022-04-06 23:41:56 +0900

    Feat. Post Delete 실행시 Tag, Category update

--- back_new/src/posts/post.service.ts
+++ back_new/src/posts/post.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { CategoryService } from '@categories/category.service';
+import { TagService } from '@tags/tag.service';
 import { CreatePostInput } from './dto/createPost.dto';
 import { Post, PostModel } from './post.model';
 
 @Injectable()
 export class PostService {
-  constructor(@InjectModel(Post.name) private postModel: PostModel) {}
+  constructor(
+    @InjectModel(Post.name) private postModel: PostModel,
+    private tagService: TagService,
+    private categoryService: CategoryService,
+  ) {}
 
   // 단일 포스트
   async getById(_id: string) {
@@ -16,16 +22,29 @@ export class PostService {
     return await this.postModel.create(input);
   }
 
-  async update(_id: string, input: CreatePostInput) {
-    return await this.postModel.updateOne({ _id }, input);
+  async update(_id: string, query: any) {
+    return await this.postModel.updateOne({ _id }, query);
   }
 
   async delete(_id: string) {
-    return await this.postModel.deleteOne({ _id });
-  }
+    const deletedPost = await this.postModel.findOneAndDelete({ _id });
 
-  async test() {
-    return await this.postModel.find();
+    await deletedPost.populate('tags');
+    await deletedPost.populate('categories');
+
+    const query = {
+      $inc: { postCount: -1 },
+    };
+
+    await Promise.all(
+      deletedPost.tags.map((tag) =>
+        this.tagService.updateTag(tag._id.toString(), query),
+      ),
+    );
+
+    await this.categoryService.update(deletedPost.category._id, query);
+
+    return deletedPost;
   }
 
   // 복수 포스트
