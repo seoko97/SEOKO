d1240978de309e4462cebe0ff62065a21c5398e0
seokho10007 <sukho1007@naver.com>
2022-04-15 23:54:19 +0900

    Feat. Menu Click Handler 추가

--- front/src/components/icons/MenuIcon/MenuIcon.tsx
+++ front/src/components/icons/MenuIcon/MenuIcon.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const MenuIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+interface Props {
+  openMenu: () => void;
+}
+
+const MenuIcon = ({ openMenu }: Props) => (
+  <svg onClick={openMenu} width="24" height="24" viewBox="0 0 24 24">
     <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
   </svg>
 );
