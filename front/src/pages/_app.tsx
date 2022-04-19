441fd29f454d2297f81a0bdbaa7314765dfad912
seokho10007 <sukho1007@naver.com>
2022-04-19 23:33:24 +0900

    Feat. Signin User Check Action 실행

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,53 +1,54 @@
 import React, { useCallback, useEffect, useMemo } from "react";
-import axios from "axios";
 import Head from "next/head";
 import { AppContext, AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
 import { useCookies } from "react-cookie";
 import { ThemeProvider } from "@emotion/react";
 
 import cookieParser from "@lib/cookieParser";
+import { useApollo } from "@lib/apllo";
+import signinCheck from "@lib/signinCheck";
+
 import AppLayout from "@frames/AppLayout";
 import DarkModeButton from "@molecules/DarkModeButton";
+import { ApolloProvider } from "@apollo/client";
 
 import { darkTheme, lightTheme } from "@theme/.";
 import GlobalStyle from "@theme/GlobalStyle";
-import { initatialSigninCheck } from "@apis/users";
-import { initializeRecoilState } from "@states/.";
-import SigninUserHeader from "@src/components/UI/molecules/SigninUserHeader";
 
 interface Props extends AppProps {
   mode: string;
   user: string | null;
 }
 
-const SEOKO = ({ Component, pageProps, mode: modeInCookie, user }: Props) => {
+const SEOKO = ({ Component, pageProps, mode: modeInCookie }: Props) => {
   const [cookies, setCookies] = useCookies(["mode"]);
   const mode = useMemo(() => cookies.mode || modeInCookie, [cookies.mode, modeInCookie]);
-
+  const client = useApollo();
   useEffect(() => {
     if (!cookies.mode) setCookies("mode", "light");
+    signinCheck();
   }, []);
 
   const onClickDarkMode = useCallback(() => {
     setCookies("mode", mode === "light" ? "dark" : "light");
   }, [mode]);
 
+  const checkMode = useMemo(() => (mode === "light" ? lightTheme : darkTheme), [mode]);
+
   return (
     <>
       <Head>
         <title>SEOKO</title>
       </Head>
-      <RecoilRoot initializeState={initializeRecoilState(user)}>
-        <ThemeProvider theme={mode === "light" ? lightTheme : darkTheme}>
-          <GlobalStyle theme={mode === "light" ? lightTheme : darkTheme} />
-          <SigninUserHeader user={user} />
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={checkMode}>
+          <GlobalStyle theme={checkMode} />
           <AppLayout>
             <Component {...pageProps} />
             <DarkModeButton mode={mode} onClick={onClickDarkMode} />
           </AppLayout>
         </ThemeProvider>
-      </RecoilRoot>
+      </ApolloProvider>
     </>
   );
 };
@@ -56,10 +57,7 @@ SEOKO.getInitialProps = async ({ ctx }: AppContext) => {
   const cookies = ctx.req?.headers?.cookie;
   const { mode } = cookieParser(cookies);
 
-  if (ctx.req && cookies) axios.defaults.headers.common.cookie = cookies;
-  const user = await initatialSigninCheck();
-
-  return { mode: mode || "light", user };
+  return { mode: mode || "light" };
 };
 
 export default SEOKO;
