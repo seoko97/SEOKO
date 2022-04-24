a1d4b8e1bbbed8f4f3c1a0d2b0f34d459d8b0a2f
seokho10007 <sukho1007@naver.com>
2022-04-24 23:42:09 +0900

    Feat. 로그인 여부 확인 캐시 저장

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,17 +1,24 @@
 import { GetServerSideProps } from "next";
-import { post as dummyPost } from "@src/dummy/posts";
+import { posts as dummyPost } from "@src/dummy/posts";
 import { dummyCategory } from "@src/dummy/categories";
+import { intializeClinet } from "@lib/apllo";
+import { IGetUserInfo } from "@src/types/users";
+import { GET_USER_INFO } from "@queries/users";
 
 export { default } from "@pages/Home";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const posts = await dummyPost;
   const categories = await dummyCategory;
+  const apolloClient = intializeClinet({ ctx });
+
+  await apolloClient.query<IGetUserInfo>({ query: GET_USER_INFO }).catch((e) => e);
 
   return {
     props: {
       posts,
       categories,
+      initialApolloState: apolloClient.cache.extract(),
     },
   };
 };
