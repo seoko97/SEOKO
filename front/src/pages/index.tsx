36a4553bc2ef52c298b4f821cf466f0792f2ecce
seokho10007 <sukho1007@naver.com>
2022-05-02 23:53:23 +0900

    Feat. UserInfo 중복 호출 삭제

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { GetServerSideProps } from "next";
 import { intializeClinet } from "@lib/apllo";
-import { IGetUserInfo } from "@queries-types/users";
-import { GET_USER_INFO } from "@queries/users";
 import { addApolloState } from "@lib/addApolloState";
 import { IGetPosts } from "@queries-types/posts";
 import { GET_POSTS } from "@queries/post/getPosts.queries";
@@ -11,7 +9,6 @@ export { default } from "@pages/Home";
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const apolloClient = intializeClinet({ ctx });
 
-  await apolloClient.query<IGetUserInfo>({ query: GET_USER_INFO, errorPolicy: "all" });
   await apolloClient.query<IGetPosts>({ query: GET_POSTS });
 
   return addApolloState(apolloClient, {
