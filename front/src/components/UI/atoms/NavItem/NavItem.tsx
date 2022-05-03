85d10deba0093ac27eb64593fc7cf6459ec4df7b
seokho10007 <sukho1007@naver.com>
2022-05-03 15:45:06 +0900

    Chore. NavItem a tag 추가

--- front/src/components/UI/atoms/NavItem/NavItem.tsx
+++ front/src/components/UI/atoms/NavItem/NavItem.tsx
@@ -1,30 +1,18 @@
 import React from "react";
 import Link from "next/link";
-import styled from "@emotion/styled";
 
 interface Props {
   href: string;
   name: string;
 }
 
-const StyledNavItem = styled.li`
-  box-sizing: border-box;
-  padding: 30px;
-  width: 100%;
-  cursor: pointer;
-  color: ${({ theme }) => theme.FONT_COLOR.PRIMARY_COLOR};
-  transition: background-color 0.2s ease-in-out;
-
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-`;
-
 const NavItem: React.FC<Props> = ({ href, name }) => {
   return (
     <>
-      <Link href={href.toLowerCase()} prefetch={false}>
-        <StyledNavItem>{name}</StyledNavItem>
+      <Link href={href.toLowerCase()}>
+        <a>
+          <li>{name}</li>
+        </a>
       </Link>
     </>
   );
