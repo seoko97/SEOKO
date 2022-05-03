2f6551a7de2fb33a75ba18278ce1fe95f25b110a
seokho10007 <sukho1007@naver.com>
2022-05-03 15:58:01 +0900

    Feat. Editer init props 변경

--- front/src/components/UI/organisms/TuiEditor/index.tsx
+++ front/src/components/UI/organisms/TuiEditor/index.tsx
@@ -1,16 +1,55 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useCallback, useRef } from "react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
+import { Editor as EditorType, EditorProps } from "@toast-ui/react-editor";
 
 import "prismjs/themes/prism.css";
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+import styled from "@emotion/styled";
+
+interface EditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
+}
+
+interface Props extends EditorProps {
+  onChange(value: string): void;
+
+  valueType?: "markdown" | "html";
+}
 
 const TEditor = dynamic(() => import("./Editor"), { ssr: false });
-const EditorWithForwardedRef = forwardRef<any | undefined, any>((props, ref) => (
-  <TEditor {...props} forwardedRef={ref as React.MutableRefObject<any>} />
-));
+const EditorWithForwardedRef = forwardRef<EditorType | undefined, EditorPropsWithHandlers>(
+  (props, ref) => <TEditor {...props} forwardedRef={ref as React.MutableRefObject<EditorType>} />,
+);
 
-const TuiEditor = () => {
+const Container = styled.div`
+  background-color: #fff;
+  flex: 1;
+  min-height: 700px;
+`;
+
+const TuiEditor = (props: Props) => {
+  const {
+    initialValue,
+    previewStyle,
+    height,
+    initialEditType,
+    useCommandShortcut,
+    onChange,
+    valueType,
+  } = props;
+
+  const editorRef = useRef<EditorType>();
+
+  const handleChange = useCallback(() => {
+    if (!editorRef.current) return;
+
+    const instance = editorRef.current.getInstance();
+    const valueTypeC = valueType || "markdown";
+
+    onChange(valueTypeC === "markdown" ? instance.getMarkdown() : instance.getHTML());
+  }, [props, editorRef]);
+
   return (
     <>
       <Head>
@@ -28,9 +67,18 @@ const TuiEditor = () => {
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/github.min.css"
         />
       </Head>
-      <>
-        <EditorWithForwardedRef />
-      </>
+      <Container>
+        <EditorWithForwardedRef
+          {...props}
+          initialValue={initialValue || "hello react editor world!"}
+          previewStyle={previewStyle || "vertical"}
+          height={height || "700px"}
+          initialEditType={initialEditType || "markdown"}
+          useCommandShortcut={useCommandShortcut || true}
+          ref={editorRef}
+          onChange={handleChange}
+        />
+      </Container>
     </>
   );
 };
