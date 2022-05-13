2f9fd85067955256ab40453e6fcbfa30c25196e5
seokho10007 <sukho1007@naver.com>
2022-05-13 23:42:50 +0900

    Feat. Post Page 생성

--- front/src/components/pages/Post/Post.tsx
+++ front/src/components/pages/Post/Post.tsx
@@ -1,12 +1,58 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_POST } from "@queries/post/getPost.queries";
+import { IGetPost } from "@queries-types/posts";
+import styled from "@emotion/styled";
+import RowFrame from "@frames/RowFrame";
 
-const Post = () => {
+import Markdown from "@organisms/MarkDownViewer";
+import Toc from "@organisms/Toc";
+import PostHeader from "@organisms/PostHeader/PostHeader";
+
+interface Props {
+  _id: string;
+}
+
+const Container = styled(RowFrame)`
+  display: flex;
+  gap: 30px;
+  & > :first-of-type {
+    width: calc(100% - 200px);
+  }
+  & > :last-of-type {
+    flex: 1;
+  }
+
+  @media (max-width: ${({ theme }) => theme.BP.PC}) {
+    & > :first-of-type {
+      width: 100%;
+    }
+  }
+`;
+
+const Post = ({ _id }: Props) => {
+  const { data } = useQuery<IGetPost>(GET_POST, { variables: { input: { id: _id } } });
+
+  if (!data) return <></>;
+
+  const { category, content, coverImg, createdAt, tags, title } = data.getPost.post;
+
   return (
-    <>
+    <Container>
       <div>
-        <div>asdasd</div>
+        <PostHeader
+          category={category}
+          coverImg={coverImg}
+          createdAt={createdAt}
+          tags={tags}
+          title={title}
+        />
+        <Markdown content={content} />
       </div>
-    </>
+      <div>
+        <Toc content={content} />
+      </div>
+    </Container>
   );
 };
 
