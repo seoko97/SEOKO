03ad78cad745cf90eb3f174e845f087bf702f192
seokho10007 <sukho1007@naver.com>
2022-05-17 23:56:53 +0900

    Feat. CreateElement를 통한 Heading Component 생성

--- front/src/components/UI/organisms/MarkDownViewer/renderers/headings/headings.tsx
+++ front/src/components/UI/organisms/MarkDownViewer/renderers/headings/headings.tsx
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 import { HeadingComponent } from "react-markdown/lib/ast-to-react";
 import { setTocVar } from "@store/toc";
 
-const h1: HeadingComponent = ({ children, level, node }) => {
+const heading: HeadingComponent = ({ children, level, node }) => {
   useEffect(() => {
     setTocVar({ level, text: children.toString() });
   }, []);
 
-  return <h1 id={children.toString() + level}>{children}</h1>;
+  return React.createElement(node.tagName, { id: children.toString() + level }, children);
 };
 
-export default h1;
+export default heading;
--- front/src/components/UI/organisms/MarkDownViewer/renderers/headings/index.ts
+++ front/src/components/UI/organisms/MarkDownViewer/renderers/headings/index.ts
@@ -1,8 +1,16 @@
-import h1 from "./h1";
-import h2 from "./h2";
-import h3 from "./h3";
-import h4 from "./h4";
-import h5 from "./h5";
-import h6 from "./h6";
+import { Components } from "react-markdown";
+import { ReactMarkdownNames, HeadingComponent } from "react-markdown/lib/ast-to-react";
 
-export { h1, h2, h3, h4, h5, h6 };
+import heading from "./headings";
+
+interface IHeading {
+  [key: string]: HeadingComponent | ReactMarkdownNames;
+}
+
+export const headings: Components = ["h1", "h2", "h3", "h4", "h5", "h6"].reduce<IHeading>(
+  (acc, tagname) => {
+    acc[tagname] = heading;
+    return acc;
+  },
+  {},
+);
