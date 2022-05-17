5fce67c20e83b31ad51aecd023ba4a987aaf1047
seokho10007 <sukho1007@naver.com>
2022-05-17 23:58:04 +0900

    Feat. Markdown Style 저장

---
+++ front/src/components/UI/organisms/MarkDownViewer/styles.ts
@@ -0,0 +1,114 @@
+import styled from "@emotion/styled";
+
+export const MarkdownWrapper = styled.div`
+  width: 100%;
+  overflow-wrap: break-word;
+  user-select: text;
+
+  &:first-of-type {
+    margin-top: 0;
+  }
+
+  & td {
+    border: 1px solid #ccc;
+  }
+
+  & * {
+    font-size: 17px;
+  }
+
+  & pre {
+    color: ${({ theme }) => theme.FONT_COLOR.PRIMARY_COLOR};
+    border-radius: 10px;
+    overflow-wrap: break-word;
+
+    & .token {
+      background: none;
+    }
+  }
+
+  & ul {
+    list-style: disc;
+  }
+
+  & ol {
+    list-style: decimal;
+  }
+
+  & ul,
+  ol {
+    padding-left: 32px;
+  }
+
+  & li {
+    margin-bottom: 12px;
+  }
+
+  & h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  td,
+  ul,
+  ol {
+    color: ${({ theme }) => theme.FONT_COLOR.PRIMARY_COLOR};
+    overflow-wrap: break-word;
+  }
+
+  & > p,
+  & > ul,
+  & > ol,
+  & table,
+  & blockquote,
+  & pre,
+  & img {
+    margin-bottom: 24px;
+  }
+  & h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 700;
+    margin: 11px 0px 4px;
+  }
+
+  h1 {
+    margin-top: 64px;
+    font-size: 40px;
+  }
+  h2 {
+    margin-top: 64px;
+    font-size: 28px;
+    margin-bottom: 24px;
+  }
+  h3 {
+    margin-top: 48px;
+    font-size: 22px;
+    margin-bottom: 24px;
+  }
+  h4 {
+    margin-top: 36px;
+    font-size: 22px;
+    margin-bottom: 24px;
+  }
+  h5 {
+    margin-top: 24px;
+    font-size: 20px;
+    margin-bottom: 24px;
+  }
+  h6 {
+    margin-top: 24px;
+    font-size: 18px;
+  }
+  & img {
+    display: block;
+    margin: 0 auto;
+    max-width: 100%;
+  }
+`;
