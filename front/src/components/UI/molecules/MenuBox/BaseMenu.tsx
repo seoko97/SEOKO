6f4b4cfbb55c51b1e6645f20d5bdac2a89199fe7
seokho10007 <sukho1007@naver.com>
2022-06-13 23:45:55 +0900

    Feat. Props 삭제 및 내부 hook 생성

---
+++ front/src/components/UI/molecules/MenuBox/BaseMenu.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from "react";
+import MenuIcon from "@icons/MenuIcon";
+import Nav from "@molecules/Nav";
+import NavList from "@molecules/NavList";
+import useDetectOutsideClick from "@hooks/useDetectOutsideClick";
+
+interface IProps {
+  username: string | null;
+}
+
+const Menu = ({ username }: IProps) => {
+  const navRef = useRef(null);
+  const [isActive, , activeHandler] = useDetectOutsideClick(navRef, false);
+
+  return (
+    <>
+      <div onClick={activeHandler}>
+        <MenuIcon />
+      </div>
+      <Nav isActive={isActive} ref={navRef}>
+        <NavList username={username} />
+      </Nav>
+    </>
+  );
+};
+
+export default Menu;
