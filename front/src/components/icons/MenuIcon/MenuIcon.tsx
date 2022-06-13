67373e02b3c4faddeb65548fc84e5ea040d910a5
seokho10007 <sukho1007@naver.com>
2022-06-13 23:45:19 +0900

    Feat. SearchIcon 생성 및 onClick Props 제거

--- front/src/components/icons/MenuIcon/MenuIcon.tsx
+++ front/src/components/icons/MenuIcon/MenuIcon.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-interface Props {
-  menuHandler: () => void;
-}
-
-const MenuIcon = ({ menuHandler }: Props) => (
-  <svg onClick={menuHandler} width="24" height="24" viewBox="0 0 24 24">
+const MenuIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24">
     <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
   </svg>
 );
