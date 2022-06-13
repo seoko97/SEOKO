347765e0ed28bacb76532d66f7f895001903ace6
seokho10007 <sukho1007@naver.com>
2022-06-13 23:48:43 +0900

    Feat. Page 이동시 State 변경

--- front/src/hooks/useDetectOutsideClick.ts
+++ front/src/hooks/useDetectOutsideClick.ts
@@ -1,16 +1,29 @@
-import { MutableRefObject, useState, useEffect, Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/router";
+import {
+  MutableRefObject,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+} from "react";
 
-type RetrunObj = [boolean, Dispatch<SetStateAction<boolean>>];
+type RetrunObj = [boolean, Dispatch<SetStateAction<boolean>>, () => void];
 
 const useDetectOutsideClick = (
   el: MutableRefObject<Node | null>,
   initialState: boolean,
 ): RetrunObj => {
+  const router = useRouter();
   const [isActive, setIsActive] = useState(initialState);
+
+  const onChangeActive = useCallback(() => {
+    setIsActive(!isActive);
+  }, [isActive]);
 
   useEffect(() => {
     const onClick = (e: MouseEvent) => {
-      if (el.current !== null && !el.current.contains(e.target as Node)) setIsActive(!isActive);
+      if (el.current !== null && !el.current.contains(e.target as Node)) setIsActive(false);
     };
 
     if (isActive) window.addEventListener("click", onClick);
@@ -20,7 +33,11 @@ const useDetectOutsideClick = (
     };
   }, [isActive, el]);
 
-  return [isActive, setIsActive];
+  useEffect(() => {
+    setIsActive(false);
+  }, [router.asPath]);
+
+  return [isActive, setIsActive, onChangeActive];
 };
 
 export default useDetectOutsideClick;
