e6b31ea2b4207dddbbc0ab2c039cd01190e965a5
seokho10007 <sukho1007@naver.com>
2022-06-15 23:50:22 +0900

    Chore. Modal Overlay opacity 수정

--- front/src/components/modals/ModalLayout/index.tsx
+++ front/src/components/modals/ModalLayout/index.tsx
@@ -11,7 +11,7 @@ const Overlay = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -22,7 +22,6 @@ const Overlay = styled.div`
 const Container = styled.div`
   position: fixed;
   display: flex;
-  align-items: center;
   justify-content: center;
   top: 0;
   left: 0;
