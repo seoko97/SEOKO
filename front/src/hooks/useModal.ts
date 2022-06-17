925e3727dc20ba08e68f3e9c9d540efe5156cdfb
seokho10007 <sukho1007@naver.com>
2022-06-17 23:52:04 +0900

    Feat. Page 이동시 State 변경

--- front/src/hooks/useModal.ts
+++ front/src/hooks/useModal.ts
@@ -1,9 +1,11 @@
-import { Dispatch, SetStateAction, useCallback, useState } from "react";
+import { useRouter } from "next/router";
+import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
 
 type CallbackFC = () => void;
 type ReturnTypes = [boolean, CallbackFC, CallbackFC, Dispatch<SetStateAction<boolean>>];
 
 const useModal = (): ReturnTypes => {
+  const router = useRouter();
   const [isOpen, setter] = useState<boolean>(false);
 
   const onOpen = useCallback(() => {
@@ -13,6 +15,10 @@ const useModal = (): ReturnTypes => {
   const onClose = useCallback(() => {
     setter(false);
   }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [router.asPath]);
 
   return [isOpen, onOpen, onClose, setter];
 };
