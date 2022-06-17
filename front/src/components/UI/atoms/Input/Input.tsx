764f9eba78d0a986e932ae35e9ab0873a84bfcd0
seokho10007 <sukho1007@naver.com>
2022-06-17 23:50:26 +0900

    Feat. Input Props Type 변경

--- front/src/components/UI/atoms/Input/Input.tsx
+++ front/src/components/UI/atoms/Input/Input.tsx
@@ -1,32 +1,22 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-interface Props {
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  type?: string;
-  placeholder?: string;
-}
+type Props = React.ComponentProps<"input">;
 
 const StyledInput = styled.input`
   padding: 10px 14px;
   border-radius: 3px;
   border: 0;
   border: 1px solid #d2d6dc;
-  box-shadow: inset 0 2px 4px 0 hsla(0, 0%, 0%, 0.08);
+
+  :focus-visible {
+    border: 1px solid ${({ theme }) => theme.SELECTION_EFFECT_COLOR.PRIMARY_COLOR};
+    outline: 0 !important;
+  }
 `;
 
-const Input = ({ value, onChange, type, placeholder }: Props) => {
-  return (
-    <>
-      <StyledInput
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </>
-  );
+const Input = ({ value, placeholder, type, onChange }: Props) => {
+  return <StyledInput type={type} placeholder={placeholder} value={value} onChange={onChange} />;
 };
 
 Input.defaultProps = {
