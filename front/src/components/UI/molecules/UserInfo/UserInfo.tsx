7d46eeb0aaef6d61c9b2883349982c1384bc601c
seokho10007 <sukho1007@naver.com>
2022-06-17 23:55:09 +0900

    Feat. Post Detail UserInfo 생성

---
+++ front/src/components/UI/molecules/UserInfo/UserInfo.tsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+import UserAvatar from "@molecules/UserAvatar";
+import React from "react";
+
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  opacity: 1;
+  cursor: pointer;
+  transition: 0.3s color;
+  &:hover {
+    color: ${({ theme }) => theme.SELECTION_EFFECT_COLOR.PRIMARY_COLOR};
+  }
+
+  @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
+    & span {
+      display: none;
+    }
+  }
+`;
+
+interface IProps {
+  onClick: () => void;
+}
+
+const UserInfo = ({ onClick }: IProps) => {
+  return (
+    <UserContainer onClick={onClick}>
+      <UserAvatar width={30} height={30} />
+      <span>지석호</span>
+    </UserContainer>
+  );
+};
+
+export default UserInfo;
