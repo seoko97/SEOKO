c9e2eac8787c924c59a78fbca7653856b251a684
seokho10007 <sukho1007@naver.com>
2022-06-19 23:17:37 +0900

    Feat. Component 구조 개선

--- front/src/components/UI/organisms/SignInForm/SignInForm.tsx
+++ front/src/components/UI/organisms/SignInForm/SignInForm.tsx
@@ -1,39 +1,112 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import styled from "@emotion/styled";
-import Card from "@atoms/Card";
-import RowFrame from "@frames/RowFrame";
-import SignInFormSection from "./SignInFormSection";
+import { useRouter } from "next/router";
 
-const StyledSignInForm = styled.div`
-  width: 100%;
+import { useMutation, useReactiveVar } from "@apollo/client";
 
-  & > div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: calc(100vh - 244px);
+import Input from "@atoms/Input";
+import Button from "@atoms/Button";
 
-    color: ${({ theme }) => theme.FONT_COLOR.PRIMARY_COLOR};
+import useInput from "@hooks/useInput";
+import { setUserInfo, userInfoVar } from "@store/userInfo";
+import { SIGN_IN } from "@queries/users";
+import { ISignIn } from "@queries-types/users";
+
+const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  padding: 30px 10px;
+  box-sizing: border-box;
+  border-top: 4px solid ${({ theme }) => theme.BUTTON_COLOR.PRIMARY_COLOR};
+  background-color: ${({ theme }) => theme.BAKCGROUND_COLOR.SECONDARY_COLOR};
+  box-shadow: 0 1px 6px 0 hsla(0, 0%, 0%, 0.1);
+
+  & > h1 {
+    font-weight: 600;
+    text-align: center;
+  }
+
+  & div {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 15px 10px;
+    font-size: 18px;
+
+    & > input {
+      box-sizing: border-box;
+      width: 100%;
+      &:first-of-type {
+        margin-bottom: 16px;
+      }
+    }
+    & > button {
+      width: 100%;
+      padding: 12px 0;
+    }
+    &:last-of-type {
+      font-size: 16px;
+      font-weight: 600;
+      text-align: center;
+      padding: 0 10px;
+    }
   }
 
   @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
-    & > div {
-      & div {
-        width: 100%;
-      }
-    }
+    width: 100%;
   }
 `;
 
 const SignInForm = () => {
+  const router = useRouter();
+  const [userId, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const { username } = useReactiveVar(userInfoVar);
+  const [signin, { loading }] = useMutation<ISignIn>(SIGN_IN, {
+    onCompleted({ signin }) {
+      const { ok, username } = signin;
+      if (ok) setUserInfo(username);
+    },
+    onError(error) {
+      alert(error.message);
+    },
+  });
+
+  useEffect(() => {
+    if (username) router.push("/");
+  }, [username]);
+
+  const onClickSignInBtn = useCallback(
+    async (e) => {
+      e.preventDefault();
+      if (userId.length === 0) return alert("아이디를 입력하세요");
+      if (password.length === 0) return alert("비밀번호를 입력하세요");
+
+      await signin({
+        variables: {
+          input: { userId, password },
+        },
+      });
+    },
+    [userId, password],
+  );
+
   return (
-    <StyledSignInForm>
-      <RowFrame>
-        <Card>
-          <SignInFormSection />
-        </Card>
-      </RowFrame>
-    </StyledSignInForm>
+    <Container onSubmit={onClickSignInBtn}>
+      <h1>로그인</h1>
+      <div>
+        <Input value={userId} onChange={onChangeId} placeholder="아이디" />
+        <Input
+          value={password}
+          onChange={onChangePassword}
+          placeholder="비밀번호"
+          type="password"
+        />
+      </div>
+      <div>
+        <Button loading={loading} content="로그인" />
+      </div>
+    </Container>
   );
 };
 
