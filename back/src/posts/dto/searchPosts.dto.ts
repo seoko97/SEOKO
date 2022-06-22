24a233348f9266d14cb9ccef877b0592c3e0e4a9
seokho10007 <sukho1007@naver.com>
2022-06-22 23:46:22 +0900

    Feat. Args LastId 상속

--- back/src/posts/dto/searchPosts.dto.ts
+++ back/src/posts/dto/searchPosts.dto.ts
@@ -1,9 +1,10 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreRes } from '@decorators/coreRes.decorator';
+import { GetPostsInput } from './getPosts.dto';
 import { Post } from '../post.model';
 
 @InputType()
-export class SearchPostsInput {
+export class SearchPostsInput extends GetPostsInput {
   @Field(() => String)
   text: string;
 }
