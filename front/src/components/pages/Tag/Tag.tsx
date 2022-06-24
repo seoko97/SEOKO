5b71fd03f60e8c00c5d1e5fd7fe20094d168ad59
seokho10007 <sukho1007@naver.com>
2022-06-25 02:21:35 +0900

    Feat. Tag Page 생성

--- front/src/components/pages/Tag/Tag.tsx
+++ front/src/components/pages/Tag/Tag.tsx
@@ -1,28 +1,49 @@
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { memo, useCallback, useRef } from "react";
+import RowFrame from "@frames/RowFrame";
+import { ITag } from "@queries-types/tags";
+import { OperationVariables, useQuery } from "@apollo/client";
+import { GET_POSTS_BY_TAG } from "@queries/post/getPostsByTag.queries";
+import { IGetPostsByTag } from "@queries-types/posts";
+import PostList from "@organisms/PostList";
+import TagListForm from "@organisms/TagListForm";
 
-const Tag = () => {
-  const router = useRouter();
-  const [selectedTag, setSelectedTag] = useState<string>("");
+interface IProps {
+  tagName: string | undefined;
+  tags: ITag[];
+}
 
-  useEffect(() => {
-    const { name } = router.query;
-    if (typeof name === "string") setSelectedTag(name);
-    else if (typeof name === "object")
-      setSelectedTag(
-        name.reduce((text, cur) => {
-          text += cur;
-          return text;
-        }, ""),
-      );
-  }, []);
+const Tag = ({ tags, tagName }: IProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { data, fetchMore } = useQuery<IGetPostsByTag>(GET_POSTS_BY_TAG, {
+    variables: tagName
+      ? {
+          input: {
+            tagName,
+          },
+        }
+      : {},
+  });
+
+  const getMorePosts = useCallback(() => {
+    const variables: OperationVariables = {
+      input: {
+        lastId: data?.getPostsByTag.posts[data?.getPostsByTag.posts.length - 1]._id,
+      },
+    };
+
+    if (tagName) variables.input.tagName = tagName;
+
+    fetchMore({ variables });
+  }, [tagName, data]);
 
   return (
-    <div>
-      <div>태그 리스트</div>
-      <div>포스트 리스트</div>
-    </div>
+    <RowFrame>
+      <TagListForm tags={tags} tagName={tagName} />
+      {data?.getPostsByTag?.posts[0] && (
+        <PostList func={getMorePosts} ref={ref} posts={data?.getPostsByTag.posts} />
+      )}
+    </RowFrame>
   );
 };
 
-export default Tag;
+export default memo(Tag, (prev, next) => prev.tagName === next.tagName);
