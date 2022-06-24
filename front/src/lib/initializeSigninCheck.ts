9412fe560d70e7d72b146c9604f512c361829a35
seokho10007 <sukho1007@naver.com>
2022-06-25 02:33:45 +0900

    Feat. User 정보 Caching

--- front/src/lib/initializeSigninCheck.ts
+++ front/src/lib/initializeSigninCheck.ts
@@ -1,15 +1,14 @@
 import { GET_USER_INFO } from "@queries/users";
 import { setUserInfo } from "@store/userInfo";
 import { IGetUserInfo } from "@queries-types/users";
-import { intializeClinet } from "./apllo";
+import { intializeClient } from "./apllo";
 
 const initializeSigninCheck = async () => {
-  const apolloClient = intializeClinet();
+  const apolloClient = intializeClient();
 
   const { data } = await apolloClient.query<IGetUserInfo>({
     query: GET_USER_INFO,
     errorPolicy: "all",
-    fetchPolicy: "no-cache",
   });
 
   if (data?.getUserInfo?.ok) setUserInfo(data.getUserInfo.username);
