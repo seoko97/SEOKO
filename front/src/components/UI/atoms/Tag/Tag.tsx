b3feef169d6e5557ce022b689bc1e3c9da58cbda
seokho10007 <sukho1007@naver.com>
2022-06-25 02:42:04 +0900

    Style. Tag font size 변경

--- front/src/components/UI/atoms/Tag/Tag.tsx
+++ front/src/components/UI/atoms/Tag/Tag.tsx
@@ -7,15 +7,19 @@ interface Props {
 }
 
 const StyledTag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   padding: 6px 8px;
   color: ${({ theme }) => theme.FONT_COLOR.LOGO_COLOR};
   border: 1px solid ${({ theme }) => theme.FONT_COLOR.LOGO_COLOR};
   font-weight: 400;
   border-radius: 10px;
   transition: background-color 0.3s;
+
   cursor: pointer;
-  font-size: 14.4px;
-  line-height: 100%;
+  font-size: 12px;
+  line-height: 16px;
 
   &:hover {
     background: ${({ theme }) => theme.SELECTION_EFFECT_COLOR.SECONDARY_COLOR};
