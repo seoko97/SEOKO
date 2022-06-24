76a3f2700ff3e1fe551ffca0f908fefb1c4d0af4
seokho10007 <sukho1007@naver.com>
2022-06-25 02:35:02 +0900

    Feat. getTags, getPostsByTag Query 실행 후 Caching

--- front/src/pages/tag/[name]/index.ts
+++ front/src/pages/tag/[name]/index.ts
@@ -1,14 +1,17 @@
 import { GetServerSideProps } from "next";
+
 import { addApolloState } from "@lib/addApolloState";
 import { intializeClient } from "@lib/apllo";
-import { IGetPostsByTag } from "@queries-types/posts";
+
 import { IGetTags } from "@queries-types/tags";
-import { GET_POSTS_BY_TAG } from "@queries/post/getPostsByTag.queries";
+import { IGetPostsByTag } from "@queries-types/posts";
 import { GET_TAGS } from "@queries/tag/getTags.queries";
+import { GET_POSTS_BY_TAG } from "@queries/post/getPostsByTag.queries";
 
 export { default } from "@pages/Tag";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { name } = ctx.query;
   const apolloClient = intializeClient({ ctx });
 
   const { data: tagsData } = await apolloClient.query<IGetTags>({
@@ -17,12 +20,13 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   await apolloClient.query<IGetPostsByTag>({
     query: GET_POSTS_BY_TAG,
+    variables: { input: { tagName: name as string } },
     errorPolicy: "all",
   });
 
   return addApolloState(apolloClient, {
     props: {
-      tagName: null,
+      tagName: name,
       tags: tagsData.getTags.tags.slice().sort((a, b) => b.posts.length - a.posts.length) ?? [],
     },
   });
