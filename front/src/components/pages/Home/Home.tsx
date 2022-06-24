cd0b4cb869c7107ed1b2290db5378fe86770d6cd
seokho10007 <sukho1007@naver.com>
2022-06-25 02:20:57 +0900

    Feat. Infinity Scroll 적용

--- front/src/components/pages/Home/Home.tsx
+++ front/src/components/pages/Home/Home.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 
 import RowFrame from "@frames/RowFrame";
 import Intro from "@molecules/Intro";
 
 import PostList from "@organisms/PostList";
+import { useQuery } from "@apollo/client";
+import { IGetPosts } from "@queries-types/posts";
+import { GET_POSTS } from "@queries/post/getPosts.queries";
 
 const Home = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { data, fetchMore } = useQuery<IGetPosts>(GET_POSTS, {
+    errorPolicy: "all",
+  });
+
+  const fetchMorePosts = useCallback(() => {
+    fetchMore({
+      variables: {
+        input: {
+          lastId: data?.getPosts.posts[data?.getPosts.posts.length - 1]._id,
+        },
+      },
+    });
+  }, [ref, data]);
+
   return (
     <RowFrame>
       <Intro />
-      <PostList />
+      {data?.getPosts?.posts[0] && (
+        <PostList ref={ref} posts={data?.getPosts.posts} func={fetchMorePosts} />
+      )}
     </RowFrame>
   );
 };
