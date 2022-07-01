bf85be25052e5a939b80865b5e78273d8c1607e3
seokho10007 <sukho1007@naver.com>
2022-07-01 23:35:16 +0900

    Chore. Info Icon 추가

---
+++ front/src/components/icons/MailIcon/MailIcon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MailIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+    <path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z" />
+  </svg>
+);
+
+export default MailIcon;
