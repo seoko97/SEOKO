36ce5d300d56d5d19006083b7c73db8f16d5a5f4
seokho10007 <sukho1007@naver.com>
2022-07-02 02:31:55 +0900

    Feat. GET_ABUT Query 호출

--- front/src/pages/about/index.ts
+++ front/src/pages/about/index.ts
@@ -1,1 +1,19 @@
+import { addApolloState } from "@lib/addApolloState";
+import { intializeClient } from "@lib/apllo";
+import { GET_ABOUT } from "@queries/about";
+import { GetServerSideProps } from "next";
+
 export { default } from "@pages/About";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apolloClient = intializeClient({ ctx });
+
+  await apolloClient.query({
+    query: GET_ABOUT,
+    errorPolicy: "all",
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+  });
+};
