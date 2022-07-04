ba28f4109cc8be623f26915e87cff497f0cd0c78
seokho10007 <sukho1007@naver.com>
2022-07-04 23:37:59 +0900

    Feat. NextJs Build Module 설정

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const withTM = require("next-transpile-modules")(["react-syntax-highlighter"]);
+const Dotenv = require("dotenv-webpack");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+// const CompressionPlugin = require("compression-webpack-plugin");
+
+const prod = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  webpack: (config) => {
+    const plugins = [...config.plugins, new Dotenv({ silent: true })];
+
+    // if (prod) {
+    //   plugins.push(new CompressionPlugin());
+    // }
+
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      plugins,
+    };
+  },
+  images: {
+    domains: ["velog.velcdn.com", "image.toast.com"],
+  },
 };
+
+module.exports = withTM(withBundleAnalyzer(nextConfig));
