513393072c1419ecb84171e2984bfa44789f6f8f
seokho10007 <sukho1007@naver.com>
2022-10-20 23:51:32 +0900

    Feat. Date 형식 변경 및 리턴값 변경

--- front/src/lib/dateTimeParser.ts
+++ front/src/lib/dateTimeParser.ts
@@ -1,6 +1,7 @@
 type Props = (date: string) => string;
-export const dateTimeParser: Props = (date) => {
-  const splitDate = date.substring(2, 10).split("-");
 
-  return `${splitDate[0]}년 ${splitDate[1]}월 ${splitDate[2]}일`;
+export const dateTimeParser: Props = (date) => {
+  const splitDate = date.substring(0, 10).split("-");
+
+  return `${splitDate[0]}.${splitDate[1]}.${splitDate[2]}`;
 };
