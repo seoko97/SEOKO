b3a15e5028874fc5a3798e71d095eeb3862587e9
seokho10007 <sukho1007@naver.com>
2022-11-27 22:56:59 +0900

    Feat. Typename 삭제를 위한 removeType 함수 생성

---
+++ front/src/lib/removeTypename.ts
@@ -0,0 +1,13 @@
+interface IProps {
+  __typename?: string;
+}
+
+export const removeTypename = <T extends IProps>(data: T | undefined) => {
+  if (!data) return;
+
+  const newData = { ...data };
+
+  if (newData.__typename) delete newData.__typename;
+
+  return newData;
+};
