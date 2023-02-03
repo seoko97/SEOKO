2f4eaf38cea41308f907a0086964a369afb978cc
seokho10007 <sukho1007@naver.com>
2023-02-04 03:01:16 +0900

    chore. common 폴더 module file 분리

--- back/src/common/module/mongo.module.ts
+++ back/src/common/module/mongo.module.ts
@@ -10,7 +10,6 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: `mongodb://${configService.get('DB_HOST')}:${configService.get(
           'DB_PORT',
         )}`,
-
         dbName: configService.get('DB_NAME'),
         user: configService.get('DB_USERNAME'),
         pass: configService.get('DB_PASSWORD'),
