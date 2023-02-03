7125934d99e0e3c45608631fcfd0eca1297bc3ab
seokho10007 <sukho1007@naver.com>
2023-02-04 03:46:51 +0900

    Feat. Generic을 통한 함수 속성 변경

--- front/src/lib/mergeItem.ts
+++ front/src/lib/mergeItem.ts
@@ -1,7 +1,10 @@
-export const mergeItem = (existing: any, incoming: any) => {
+import { IBasePosts } from "@queries-types/posts";
+
+export const mergeItem = <T extends IBasePosts>(existing: T, incoming: T) => {
   if (!existing) return incoming;
 
   const newPosts = [...existing.posts, ...incoming.posts];
+
   return {
     ...incoming,
     posts: newPosts,
