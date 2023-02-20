49d002334c56286760034ca6dab82bf427937605
seokho10007 <sukho1007@naver.com>
2023-02-21 00:02:46 +0900

    Refactor. Search Page 관련 코드 제거

--- front/src/queries/post/index.ts
+++ front/src/queries/post/index.ts
@@ -3,6 +3,5 @@ import { DELETE_POST } from "./deletePost.queries";
 import { EDIT_POST } from "./editPost.queries";
 import { GET_POST } from "./getPost.queries";
 import { GET_POSTS } from "./getPosts.queries";
-import { SEARCH_POSTS } from "./searchPosts.queries";
 
-export { ADD_POST, DELETE_POST, EDIT_POST, GET_POST, GET_POSTS, SEARCH_POSTS };
+export { ADD_POST, DELETE_POST, EDIT_POST, GET_POST, GET_POSTS };
