3d0724cdf12547a8e139a4c8267cac4e5e8489ed
seokho10007 <sukho1007@naver.com>
2023-02-21 00:09:58 +0900

    Style. Input Component 스타일 변경

--- front/src/components/UI/atoms/Input/Input.tsx
+++ front/src/components/UI/atoms/Input/Input.tsx
@@ -4,17 +4,19 @@ import styled from "@emotion/styled";
 type Props = React.ComponentProps<"input">;
 
 const StyledInput = styled.input`
+  width: 100%;
+
   padding: 0.5rem 2rem 0.5rem 1rem;
   border-radius: 3px;
   transition: border-color 0.2s, background-color 0.2s;
 
-  border: 1px solid #b4bac2;
   color: ${({ theme }) => theme.FONT_COLOR.PRIMARY_COLOR};
+  border: 1px solid ${({ theme }) => theme.BORDER_COLOR.PRIMARY_COLOR};
   background-color: ${({ theme }) => theme.BACKGROUND_COLOR.SECONDARY_COLOR};
 
-  :focus-visible {
-    border-color: ${({ theme }) => theme.SELECTION_EFFECT_COLOR.PRIMARY_COLOR};
+  &:focus-visible {
     outline: 0 !important;
+    border-color: ${({ theme }) => theme.SELECTION_EFFECT_COLOR.PRIMARY_COLOR};
   }
 `;
 
