d4fa13b84e45b72aae7b21ad73e90f7f39060242
seokho10007 <sukho1007@naver.com>
2023-04-21 23:19:21 +0900

    feat: success interceptor를 통한 중복 코드 삭제

---
+++ back/src/common/interseptors/success.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class SuccessInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        console.log(data);
+        return {
+          ok: true,
+          ...data,
+        };
+      }),
+    );
+  }
+}
