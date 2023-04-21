246c131449de61ad39a0d1d90c808f680f221d5f
seokho10007 <sukho1007@naver.com>
2023-04-21 22:11:07 +0900

    feat: repository 패턴을 통힌 로직 분리

--- back/src/posts/post.repository.ts
+++ back/src/posts/post.repository.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Post, PostModel } from './post.model';
+import { Post, PostDocument, PostModel } from './post.model';
 import { EditPostArgs } from './dto/editPostInput.dto';
+import { FilterQuery } from 'mongoose';
+import { CreatePostInput } from './dto/createPostInput.dto';
 
 @Injectable()
 export class PostRepository {
   constructor(@InjectModel(Post.name) private readonly postModel: PostModel) {}
 
-  async createPost(post: Post) {
-    return this.postModel.create(post);
+  async createPost(post: Omit<CreatePostInput, 'tags'>) {
+    return await this.postModel.create(post);
   }
 
-  async getPostById(_id: string) {
-    return this.postModel.findById(_id).populate('tags');
+  async deletePost(_id: string) {
+    await this.postModel.deleteOne({ _id });
   }
 
   async updatePost(input: EditPostArgs) {
@@ -28,7 +30,7 @@ export class PostRepository {
       { $addToSet: { tags: { $each: addTags } } },
     );
 
-    const updatedPost = await this.postModel.findById(_id).populate('tags');
+    const updatedPost = await this.getPostById(_id);
 
     return updatedPost;
   }
@@ -38,5 +40,34 @@ export class PostRepository {
       { 'tags.posts': { $size: 0 } },
       { $pull: { tags: { posts: { $size: 0 } } } },
     );
+  }
+
+  async getPostById(_id: string) {
+    return this.postModel.findById(_id).populate('tags');
+  }
+
+  async getSiblingPost(_id: string) {
+    return await Promise.all([
+      this.postModel
+        .findOne({
+          _id: { $lt: _id },
+          isTemporary: false,
+        })
+        .sort({ _id: -1 }),
+      this.postModel
+        .findOne({
+          _id: { $gt: _id },
+          isTemporary: false,
+        })
+        .sort({ _id: 1 }),
+    ]);
+  }
+
+  async getPosts(options: FilterQuery<PostDocument>, limit: number) {
+    return this.postModel
+      .find(options)
+      .sort({ _id: -1 })
+      .limit(limit)
+      .populate('tags');
   }
 }
--- back/src/posts/post.service.ts
+++ back/src/posts/post.service.ts
@@ -1,56 +1,30 @@
-import {
-  BadRequestException,
-  forwardRef,
-  Inject,
-  Injectable,
-} from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Types } from 'mongoose';
+import { NotFoundException, Inject, Injectable } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { TagService } from '@tags/tag.service';
-import { Post, PostDocument, PostModel } from './post.model';
+import { Post, PostModel } from './post.model';
 import { CreatePostInput } from './dto/createPostInput.dto';
 import { EditPostInput } from './dto/editPostInput.dto';
 import { GetPostsInput } from './dto/getPosts.dto';
+import { PostRepository } from './post.repository';
+import { getQueryOptionsByPost } from '@utils/getQueryOptionsByPost';
 
 @Injectable()
 export class PostService {
   constructor(
     @InjectModel(Post.name) private postModel: PostModel,
-    @Inject(forwardRef(() => TagService))
-    private tagService: TagService,
+    @Inject(TagService) private tagService: TagService,
+    private readonly postRepository: PostRepository,
   ) {}
 
-  getWhere(lastId: string | undefined): FilterQuery<PostDocument> {
-    const where = lastId ? { _id: { $lt: lastId } } : {};
-
-    return where;
-  }
-
-  async getById(_id: string) {
-    return await this.postModel.findById(_id);
-  }
-
   async createPost({ tags, ...info }: CreatePostInput) {
-    const post = await this.postModel.create({ ...info });
+    const post = await this.postRepository.createPost({ ...info });
 
-    if (!tags.length) {
-      await post.save();
-      return post;
-    }
-
-    const createdTag = await Promise.all(
-      tags.map(async (tagName) => {
-        const tag = await this.tagService.findOrCreate(tagName);
-
-        tag.posts.push(post._id);
-
-        await tag.save();
-
-        return tag;
-      }),
+    const createdTag = await this.tagService.pushAndReturnTagsByPostId(
+      tags,
+      post._id,
     );
 
-    if (createdTag.length) post.tags = createdTag;
+    post.tags = createdTag;
 
     await post.save();
     await post.populate('tags');
@@ -58,73 +32,38 @@ export class PostService {
     return post;
   }
 
-  async updatePost(_id: string | Types.ObjectId, query: any) {
-    return await this.postModel.updateOne({ _id }, query);
-  }
+  async editPost(input: EditPostInput) {
+    const { _id, addTags, deleteTags } = input;
 
-  async editPost({ _id, addTags, deleteTags, ...info }: EditPostInput) {
-    const post = await this.postModel.findOne({ _id }).populate('tags');
+    const post = await this.postModel.findOne({ _id });
 
-    if (!post) throw new Error('포스트가 존재하지 않습니다.');
+    if (!post) throw new NotFoundException('포스트가 존재하지 않습니다.');
 
-    // 삭제된 테그들을 포스트에서 제거/태그에서 포스트 제거/태그가 비어있으면 삭제
-    if (deleteTags.length) {
-      await Promise.all(
-        post.tags.map((tag) => {
-          if (!deleteTags.includes(tag.name)) return;
+    const [dTags, aTags] = await Promise.all([
+      this.tagService.deletePostIdInTags(deleteTags, post._id),
+      this.tagService.pushAndReturnTagsByPostId(addTags, post._id),
+    ]);
 
-          return this.tagService.updateTag(tag._id, {
-            $pull: { posts: post._id },
-          });
-        }),
-      );
+    const updatedPost = await this.postRepository.updatePost({
+      ...input,
+      addTags: aTags,
+      deleteTags: dTags,
+    });
 
-      post.tags = post.tags.filter((tag) => {
-        return !deleteTags.includes(tag.name);
-      });
+    await this.postRepository.updateManyByEmptyPosts();
 
-      await this.tagService.deleteEmptyTags();
-    }
-
-    if (addTags.length) {
-      const createdTag = await Promise.all(
-        addTags.map(async (tagName) => {
-          const tag = await this.tagService.findOrCreate(tagName);
-
-          tag.posts.push(post._id);
-
-          await tag.save();
-
-          return tag;
-        }),
-      );
-
-      post.tags.push(...createdTag);
-    }
-
-    for (const key in info) {
-      post[key] = info[key];
-    }
-
-    await post.save();
-
-    return post;
+    return updatedPost;
   }
 
   async deletePost(_id: string) {
-    await this.postModel.deleteOne({ _id });
-
+    await this.postRepository.deletePost(_id);
     await this.tagService.deleteManyByPostId(_id);
-
-    return { ok: true };
   }
 
   async getPost(_id: string) {
-    const post = await this.postModel.findOne({ _id });
+    const post = await this.postRepository.getPostById(_id);
 
-    if (!post) throw new Error('포스트가 존재하지 않습니다.');
-
-    await post.populate('tags');
+    if (!post) throw new NotFoundException('포스트가 존재하지 않습니다.');
 
     return post;
   }
@@ -132,80 +71,28 @@ export class PostService {
   async getSiblingPost(_id: string) {
     const post = await this.postModel.findOne({ _id });
 
-    if (!post) throw new BadRequestException('포스트가 존재하지 않습니다.');
+    if (!post) throw new NotFoundException('포스트가 존재하지 않습니다.');
 
-    const [prev, next] = await Promise.all([
-      this.postModel
-        .findOne({
-          _id: { $lt: _id },
-          isTemporary: false,
-        })
-        .sort({ createdAt: -1 }),
-      this.postModel
-        .findOne({
-          _id: { $gt: _id },
-          isTemporary: false,
-        })
-        .sort({ createdAt: 1 }),
-    ]);
+    const [prev, next] = await this.postRepository.getSiblingPost(_id);
 
     return { prev, next };
   }
 
   async getPosts(input: GetPostsInput) {
-    const {
-      lastId,
-      limit = 10,
-      tag: tagName,
-      isTemporary,
-      ...options
-    } = input ?? {};
+    const { limit = 10, tag: tagName } = input ?? {};
 
-    const where: FilterQuery<PostDocument> = {
-      ...options,
-      ...this.getWhere(lastId),
-      isTemporary: isTemporary ?? false,
-    };
+    const where = getQueryOptionsByPost(input);
 
     if (tagName) {
       const tag = await this.tagService.getTag(tagName);
 
-      if (!tag) throw new BadRequestException('존재하지 않는 태그입니다.');
+      if (!tag) throw new NotFoundException('존재하지 않는 태그입니다.');
 
       where.tags = {
         $in: tag._id,
       };
     }
 
-    return this.postModel
-      .find(where)
-      .sort({ _id: -1 })
-      .limit(limit)
-      .populate('tags');
-  }
-
-  async searchPosts(input: GetPostsInput) {
-    const { text, lastId } = input;
-
-    if (!text.length) return [];
-
-    const where: FilterQuery<PostDocument> = {
-      $and: [
-        {
-          $or: [{ title: { $regex: text } }, { content: { $regex: text } }],
-        },
-        this.getWhere(lastId),
-      ],
-      ...input,
-    };
-
-    return this.getPosts(where);
-  }
-
-  async deleteManyByTagId(tagId: string) {
-    return await this.postModel.updateMany(
-      { tags: { $in: tagId } },
-      { $pull: { tags: tagId } },
-    );
+    return this.postRepository.getPosts(where, limit);
   }
 }
