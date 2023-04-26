f08bbf35de02d75cec579a187c481df5db5dc3d2
seokho10007 <sukho1007@naver.com>
2023-04-27 00:10:13 +0900

    feat: ConnectionStore에 connection 추가 및 plugin 적용 (#80)

--- back/src/common/module/mongo.module.ts
+++ back/src/common/module/mongo.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+import { ConnectionStore } from '@utils/ConnectionStore';
+import { transactionPlugin } from '@utils/mongoosePluginCb';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: `mongodb://${configService.get('DB_HOST')}:${configService.get(
-          'DB_PORT',
-        )}`,
+        uri: configService.get('DB_URI'),
         dbName: configService.get('DB_NAME'),
         user: configService.get('DB_USERNAME'),
         pass: configService.get('DB_PASSWORD'),
+
+        connectionFactory: (connection: Connection) => {
+          connection.plugin(transactionPlugin);
+
+          new ConnectionStore().setConnection(connection);
+
+          return connection;
+        },
       }),
       inject: [ConfigService],
     }),
