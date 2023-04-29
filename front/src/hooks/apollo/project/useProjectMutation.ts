c0116293c9f1f18382fba86ed4445b192e5f7f92
seokho10007 <sukho1007@naver.com>
2023-04-30 01:13:14 +0900

    feat: mutation 시 cache 업데이트 (#79)

--- front/src/hooks/apollo/project/useProjectMutation.ts
+++ front/src/hooks/apollo/project/useProjectMutation.ts
@@ -1,13 +1,26 @@
-import { useMutation } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import {
   IAddProject,
   IAddProjectVariables,
+  IDeleteProject,
   IEditProject,
   IProject,
   IProjectInput,
 } from "@queries-types/project";
-import { ADD_PROJECT, EDIT_PROJECT } from "@queries/project";
+import { ADD_PROJECT, DELETE_PROJECT, EDIT_PROJECT } from "@queries/project";
 import { useRouter } from "next/router";
+
+const PROJECT_FRAGMENT = gql`
+  fragment ProjectFragment on Project {
+    _id
+    title
+    description
+    coverImg
+    startDate
+    endDate
+    isTemporary
+  }
+`;
 
 const useProjectMutation = (project?: IProject) => {
   const router = useRouter();
@@ -18,10 +31,25 @@ const useProjectMutation = (project?: IProject) => {
 
   const [addProjectMutation] = useMutation<IAddProject, IAddProjectVariables>(ADD_PROJECT, {
     onCompleted,
+    update: (cache) => {
+      cache.evict({ fieldName: "getProjects" });
+      cache.gc();
+    },
   });
 
   const [editProjectMutation] = useMutation<IEditProject, IAddProjectVariables>(EDIT_PROJECT, {
     onCompleted,
+    update: (cache, { data }) => {
+      if (!data?.editProject) return;
+
+      const { project } = data.editProject;
+
+      cache.writeFragment({
+        id: `Project:${project._id}`,
+        fragment: PROJECT_FRAGMENT,
+        data: project,
+      });
+    },
   });
 
   const onMutation = async (input: IProjectInput) => {
@@ -33,4 +61,28 @@ const useProjectMutation = (project?: IProject) => {
   return [onMutation];
 };
 
-export { useProjectMutation };
+const useDeleteProject = (_id: string) => {
+  const router = useRouter();
+
+  const onCompleted = () => {
+    router.push("/project");
+  };
+
+  const [deleteProjectMutation] = useMutation<IDeleteProject>(DELETE_PROJECT, {
+    onCompleted,
+    update: (cache) => {
+      const id = cache.identify({ _id, __typename: "Project" });
+
+      cache.evict({ id });
+      cache.gc();
+    },
+  });
+
+  const onDelete = async () => {
+    await deleteProjectMutation({ variables: { input: { _id } } });
+  };
+
+  return [onDelete];
+};
+
+export { useProjectMutation, useDeleteProject };
