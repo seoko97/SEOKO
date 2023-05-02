8422f12bf61f722523bc46e5a66d49136fa36824
seokho10007 <sukho1007@naver.com>
2023-05-03 01:15:49 +0900

    feat: MarkDown style 변경

--- front/src/components/UI/organisms/MarkDownViewer/renderers/index.ts
+++ front/src/components/UI/organisms/MarkDownViewer/renderers/index.ts
@@ -1,11 +1,13 @@
 import { Components } from "react-markdown";
 import code from "./code";
 import blockquote from "./blockquote";
+import img from "./img";
 import { headings } from "./headings";
 
 const renderers: Components = {
   code,
   blockquote,
+  img,
   ...headings,
 };
 
--- front/src/components/UI/organisms/MarkDownViewer/renderers/inlineCode.tsx
+++ front/src/components/UI/organisms/MarkDownViewer/renderers/inlineCode.tsx
@@ -4,9 +4,12 @@ import { CodeComponent } from "react-markdown/lib/ast-to-react";
 
 const InlineCode = styled.code`
   background-color: ${({ theme }) => theme.MARK_DOWN.BACK_COLOR};
-  border-radius: 4px;
-  padding: 2px 4.8px;
   color: ${({ theme }) => theme.MARK_DOWN.CODE_COLOR};
+
+  transition: background-color 0.3s, color 0.3s !important;
+
+  border-radius: 4px;
+  padding: 3px 8px;
   font-weight: 600;
   font-size: 14.4px;
   overflow-wrap: anywhere;
