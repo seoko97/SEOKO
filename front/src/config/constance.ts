3ca7d5dd86a33af617853cd5112c0fc1bc38c74b
seokho10007 <sukho1007@naver.com>
2023-05-21 22:32:23 +0900

    feat: 환경변수 추가

--- front/src/config/constance.ts
+++ front/src/config/constance.ts
@@ -1,4 +1,6 @@
 const API_URL = process.env.API_URL as string;
 const isProd = process.env.NODE_ENV === "production";
+const HOST = isProd ? process.env.HOST : "http://localhost:3000";
+const GOOGLE_SITE_VERIFICATION = isProd ? process.env.GOOGLE_SITE_VERIFICATION : "";
 
-export { API_URL, isProd };
+export { API_URL, isProd, GOOGLE_SITE_VERIFICATION, HOST };
